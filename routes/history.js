834be1b42a38e4396a7e9fac16b4ee957f5b88c6
nukebomb <nukebomb@sina.cn>
2018-11-15 14:11:59 +0800

    '添加历史查询的接口'

--- routes/history.js
+++ routes/history.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var randomData = require('./randomData')
+router.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-  res.send(randomData.randomArr(10,20,3))
+router.get('/:area', function (req, res, next) {
+  let datetime = randomData.createDateArr('2017-10-21', '2018-11-17')
+  let length = randomData.computeLength('2017-10-21', '2018-11-17')
+  if (req.params.area == 'chengdu') {
+    let data = randomData.randomArr(100, 500, length)
+    res.json({
+      name: 'allcity',
+      category: datetime,
+      data
+    })
+  }
 });
+
+router.get('/all/:start/:end', function (req, res) {
+  var start = req.params.start
+  var end = req.params.end
+  let datetime = randomData.createDateArr(start, end)
+  let length = randomData.computeLength(start, end)
+  let data = randomData.randomArr(100, 500, length)
+  res.json({
+    name: 'allcityByTime',
+    category: datetime,
+    data
+  })
+})
 
 
 module.exports = router;
--- routes/randomData.js
+++ routes/randomData.js
@@ -7,13 +7,30 @@ var randomData = {
     }
     return result
   },
-  createDateArr: function (start, end, length) {
-    var result = []
-    for (let i=0; i< length; i ++) {
-      
+  createDateArr: function (start, end) {
+    var result = [start]
+    var currentStart = new Date(start)
+    var realStart = currentStart.getTime()
+    var currentEnd = new Date(end)
+    var realEnd = currentEnd.getTime()
+    var between = (realEnd - realStart) / 86400000
+    var current = null
+    for (let i = 1; i <= between; i++) {
+      current = new Date(Number(realStart) + i * 86400000)
+      var Y = current.getFullYear()
+      var M = current.getMonth() + 1 < 10 ? '0' + (current.getMonth() + 1) : (current.getMonth() + 1)
+      var D = current.getDate() < 10 ? '0' + current.getDate() : current.getDate()
+      result.push(Y + '-' + M + '-' + D)
     }
+    return result
+  },
+  computeLength(start, end) {
+    var currentStart = new Date(start)
+    var realStart = currentStart.getTime()
+    var currentEnd = new Date(end)
+    var realEnd = currentEnd.getTime()
+    return (realEnd - realStart) / 86400000
   }
 }
-
 
 module.exports = randomData;
