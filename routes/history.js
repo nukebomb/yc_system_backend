f1ecdf31d1c227967a83862023a0d6bf0ca15615
nukebomb <nukebomb@sina.cn>
2018-11-27 13:16:50 +0800

    配合首页和综合查询页面的临时更改，添加了接口

--- routes/history.js
+++ routes/history.js
@@ -11,18 +11,109 @@ router.all('*', function (req, res, next) {
 })
 
 /* GET home page. */
-router.get('/:area', function (req, res, next) {
-  let datetime = randomData.createDateArr('2017-10-21', '2018-11-17')
-  let length = randomData.computeLength('2017-10-21', '2018-11-17')
-  if (req.params.area == 'chengdu') {
-    let data = randomData.randomArr(100, 500, length)
-    res.json({
-      name: 'allcity',
-      category: datetime,
-      data
-    })
-  }
-});
+// router.get('/:area', function (req, res, next) {
+//   let datetime = randomData.createDateArr('2017-10-21', '2018-11-17')
+//   let length = randomData.computeLength('2017-10-21', '2018-11-17')
+//   if (req.params.area == 'chengdu') {
+//     let data = randomData.randomArr(100, 500, length)
+//     res.json({
+//       name: 'allcity',
+//       category: datetime,
+//       data
+//     })
+//   }
+// });
+router.get('/cdmonth', function (req, res) {
+  let datetime = randomData.createDateArr('2016-03-01', '2016-03-31')
+  res.json({
+    category: datetime,
+    data: [105.77097,
+      107.60323,
+      270.075,
+      107.86129,
+      127.97119,
+      128.53667,
+      96.73462,
+      80.51935,
+      77.47742,
+      103.93226,
+      128.88065,
+      110.71774,
+      171.35484,
+      109.70968,
+      110.01353,
+      188,
+      129.22097,
+      101.77593,
+      106.28667,
+      114.69286,
+      140.62903,
+      139.23548,
+      107.47742,
+      213.73548,
+      85.14839,
+      157.5129,
+      117.55161,
+      231.31935,
+      178.63333,
+      233.99355,
+      200.15667
+    ]
+  })
+})
+router.get('/cdsession', function (req, res) {
+  res.json({
+    category: ['2016/春','2017/春','2018/春'],
+    'spring': [
+      // '2016': [106.56808, 198.93536, 153.32776],
+      153.32776,
+      129.69376,
+      // '2017': [92.12068, 129.69376, 145.76647],
+      // '2018': [64.29779, 58.69084, 50.64688]
+      58.69084
+  ]
+  })
+})
+router.get('/cdyear', function(req, res) {
+  res.json({
+    category:['2015','2016','2017','2018'],
+    data:[251.28, 123.2217,102.4321,51.22345]
+  })
+})
+router.get('/cdyear', function (req, res) {
+  res.json({
+    category: ['2015-12', '2016-01', '2016-02', '2016-03', '2016-04', '2016-05', '2016-06', '2016-07', '2016-08', '2016-09', '2016-10', '2016-11', '2016-12', '2017-09', '2017-10', '2017-11', '2017-12', '2018-01', '2018-02', '2018-03', '2018-04', '2018-05', '2018-06', '2018-07'],
+    data: [
+      251.28,
+      106.56808,
+      198.93536,
+      153.32776,
+      91.15123,
+      88.08438,
+      86.46801,
+      79.05367,
+      93.72295,
+      96.1394,
+      92.12068,
+      129.69376,
+      145.76647,
+      50.67986,
+      53.60038,
+      67.0841,
+      85.21299,
+      64.29779,
+      58.69084,
+      50.64688,
+      50.33612,
+      40.71247,
+      34.80464,
+      24.66052
+    ]
+  })
+})
+router.get('/cdyear', function (req, res) {
+
+})
 
 router.get('/all/:start/:end', function (req, res) {
   var start = req.params.start
--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+// var randomData = require('./randomData')
+
 /* GET home page. */
 router.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,40 +13,89 @@ router.all('*', function (req, res, next) {
   next();
 })
 
-
-
 router.get('/years', function (req, res) {
   res.json({
     name: "byyear",
-    chenghuaqu: randomArr(100,500, 10),
-    gaoxinqu: randomArr(100,500, 10),
-    shuangliuqu: randomArr(100,500, 10),
-    jinniuqu: randomArr(100,500, 10),
-    wuhouqu: randomArr(100,500, 10),
-    qingyangqu: randomArr(100,500, 10),
-    quanshi: randomArr(100,500, 10)
+    // chenghuaqu: randomArr(100, 500, 10),
+// gaoxinqu: randomArr(100, 500, 10),
+// shuangliuqu: randomArr(100, 500, 10),
+// jinniuqu: randomArr(100, 500, 10),
+// wuhouqu: randomArr(100, 500, 10),
+// qingyangqu: randomArr(100, 500, 10),
+// quanshi: randomArr(100, 500, 10)
   })
 })
 
-router.get('/month/:year', function (req, res) {
+router.get('/chengdu', function(req, res) {
   res.json({
-    name: 'showByMonth',
-    year: req.params.year,
-    chenghuaqu: randomArr(100,500, 12),
-    gaoxinqu: randomArr(100,500, 12),
-    shuangliuqu: randomArr(100,500, 12),
-    jinniuqu: randomArr(100,800, 12),
-    wuhouqu: randomArr(100,500, 12),
-    qingyangqu: randomArr(100,800, 12),
-    quanshi: randomArr(100,900, 12)
+    name: 'chengduByyear',
+    allCity: [
+      251.28,
+      106.56808,
+      198.93536,
+      153.32776,
+      91.15123,
+      88.08438,
+      86.46801,
+      79.05367,
+      93.72295,
+      96.1394,
+      92.12068,
+      129.69376,
+      145.76647,
+      50.67986,
+      53.60038,
+      67.0841,
+      85.21299,
+      64.29779,
+      58.69084,
+      50.64688,
+      50.33612,
+      40.71247,
+      34.80464,
+      24.66052
+    ]
   })
 })
 
-function randomArr(start, end, length) {
-  var result = []
-  for (let i = 0; i < length; i++) {
-    result.push(Math.floor(Math.random() * (end - start)) + start)
-  }
-  return result
-}
+// router.get('/month/:year', function (req, res) {
+//   res.json({
+//     name: 'showByMonth',
+//     year: req.params.year,
+//     chenghuaqu: randomArr(100, 500, 12),
+//     gaoxinqu: randomArr(100, 500, 12),
+//     shuangliuqu: randomArr(100, 500, 12),
+//     jinniuqu: randomArr(100, 800, 12),
+//     wuhouqu: randomArr(100, 500, 12),
+//     qingyangqu: randomArr(100, 800, 12),
+//     quanshi: randomArr(100, 900, 12)
+//   })
+// })
+
+// router.get('/search/:startdata', function (req, res) {
+//   var area = JSON.parse(req.params.startdata).area
+//   if (area[0] === 'allcity') {
+//     area = '全市'
+//   } else {
+//     switch (area[1]) {
+//       case 'chenghua':
+
+//     }
+//   }
+//   var date = JSON.parse(req.params.startdata).date
+//   let datetime = randomData.createDateArr(date[0], date[1])
+//   let length = randomData.computeLength(date[0], date[1])
+//   res.json({
+//     dateCategory: datetime,
+//     data: randomData.randomArr(100, 500, length),
+//     pickedArea: area
+//   })
+// })
+// function randomArr(start, end, length) {
+//   var result = []
+//   for (let i = 0; i < length; i++) {
+//     result.push(Math.floor(Math.random() * (end - start)) + start)
+//   }
+//   return result
+// }
 module.exports = router;
