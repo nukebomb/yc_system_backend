50b401a6639d125746a971e84392da46c52de238
nukebomb <nukebomb@sina.cn>
2019-01-05 21:17:36 +0800

    区域关联性添加

--- routes/relation.js
+++ routes/relation.js
@@ -11,6 +11,46 @@ router.all('*', function (req, res, next) {
   next();
 })
 
+router.post('/init', function(req, res) {
+  res.json({
+    year: {
+      date: '2018',
+      data: [
+        { value: 0.66, name: '成华区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '双流区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '高新区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '武侯区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '青羊区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '金牛区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '天府新区' }
+      ].sort(function (a, b) { return a.value - b.value })
+    },
+    session: {
+      date: ['2018', 'spring'],
+      data: [
+        { value: 0.33, name: '成华区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '双流区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '高新区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '武侯区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '青羊区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '金牛区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '天府新区' }
+      ].sort(function (a, b) { return a.value - b.value })
+    },
+    month: {
+      date: '2018-01',
+      data: [
+        { value: 0.33, name: '成华区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '双流区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '高新区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '武侯区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '青羊区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '金牛区' },
+        { value: Math.floor(Math.random() * 100) / 100, name: '天府新区' }
+      ].sort(function (a, b) { return a.value - b.value })
+    }
+  })
+})
 router.post('/year', function(req, res) {
   res.json(
     {
